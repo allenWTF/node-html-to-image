59a063b9ccf15267a9649008b741c8107c92577c
allenWTF <niowen@mail.ru>
2020-05-13 20:47:19 +0300

    Specific changes for vkafterschool project

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ module.exports = async function({
   if (!html) {
     throw Error('You must provide an html property.')
   }
-  const browser = await puppeteer.launch({ ...puppeteerArgs, headless: false});
+  const browser = await puppeteer.launch({ ...puppeteerArgs, headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage()
   if (content) {
     const template = handlebars.compile(html)
